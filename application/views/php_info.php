93fcf05c81563f619a5fb7bc746fb1dc008f725b
wanghuan <hunter.v.wang@gmail.com>
2015-02-08 03:04:59 +0800

    add redirect

--- application/views/php_info.php
+++ application/views/php_info.php
@@ -5,6 +5,7 @@
  * Date: 2/8/15
  * Time: 01:43
  */
-$this->load->helper('url');
-redirect("http://weibo.com/lbbq");
+//$this->load->helper('url');
+//redirect("http://weibo.com/lbbq");
+phpinfo();
 
