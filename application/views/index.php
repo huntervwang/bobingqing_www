2a6c5054f721981bb950916c00b365d0a402ecbb
wanghuan <hunter.v.wang@gmail.com>
2015-03-16 11:31:11 +0800

    new index

---
+++ application/views/index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>主页</title>
+    </head>
+    <body>
+        <hr>主页<hr>
+        <h2>SVN 中文文档 Version：1.6 <link href="http://www.bobingqing.com/svn_book_1.6/">点击阅读</link></h2>
+
+    </body>
+</html>
