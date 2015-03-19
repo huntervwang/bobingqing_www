fd45d4b0591a48772832608194b6a503683ebf3a
wanghuan <hunter.v.wang@gmail.com>
2015-03-19 17:21:16 +0800

    add dsl reference

--- application/views/index.php
+++ application/views/index.php
@@ -5,6 +5,7 @@
     <body>
         <hr>主页<hr>
         <h2>SVN 中文文档 Version：1.6 <a href="http://www.bobingqing.com/svn_book_1.6/">点击阅读</a></h2>
+        <h2>Gradle DSL Reference Version：2.3 <a href="http://www.bobingqing.com/gradle_dsl_reference/">点击阅读</a></h2>
 
     </body>
 </html>
