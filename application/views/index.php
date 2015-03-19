c279a6b8040c79a7fb01f9457dd0c7b5f8da1f3b
wanghuan <hunter.v.wang@gmail.com>
2015-03-19 17:52:43 +0800

    add docs

--- application/views/index.php
+++ application/views/index.php
@@ -4,8 +4,11 @@
     </head>
     <body>
         <hr>主页<hr>
-        <h2>SVN 中文文档 Version：1.6 <a href="http://www.bobingqing.com/svn_book_1.6/">点击阅读</a></h2>
+        <h2>SVN doc Version：1.6 <a href="http://www.bobingqing.com/svn_book_1.6/">点击阅读</a></h2>
         <h2>Gradle DSL Reference Version：2.3 <a href="http://www.bobingqing.com/gradle_dsl_reference/">点击阅读</a></h2>
+        <h2>Gradle java doc Version：2.3 <a href="http://www.bobingqing.com/gradle_javadoc/">点击阅读</a></h2>
+        <h2>Gradle groovy doc Version：2.3 <a href="http://www.bobingqing.com/gradle_groovydoc/">点击阅读</a></h2>
+        <h2>Gradle user guide Version：2.3 <a href="http://www.bobingqing.com/gradle_userguide/">点击阅读</a></h2>
 
     </body>
 </html>
