60c8125003ac47bea0801153ade54ffc70d3e864
wanghuan <hunter.v.wang@gmail.com>
2015-03-20 16:29:29 +0800

    add disqu

--- application/views/index.php
+++ application/views/index.php
@@ -10,5 +10,19 @@
         <h2>Gradle groovy doc Version：2.3 <a href="http://www.bobingqing.com/gradle_groovydoc/">点击阅读</a></h2>
         <h2>Gradle user guide Version：2.3 <a href="http://www.bobingqing.com/gradle_userguide/">点击阅读</a></h2>
 
+
+        <div id="disqus_thread"></div>
+        <script type="text/javascript">
+            /* * * CONFIGURATION VARIABLES * * */
+            var disqus_shortname = 'wanghuan';
+
+            /* * * DON'T EDIT BELOW THIS LINE * * */
+            (function() {
+                var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+            })();
+        </script>
+        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
     </body>
 </html>
